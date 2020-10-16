8d9189cfe5e90724e82cc448cd6f538fbe5a7f3a
Shubham Gupta <61947842+starkex@users.noreply.github.com>
2020-10-16 21:04:47 +0530

    linking-bars

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-        <p>Heelo boiii iots first react website im gonna make </p>
-    </div>
+  <>
+  <Router>
+    <Navbar/>
+    <Switch>
+      <Route path='/' exact />
+    </Switch>
+  </Router>
+  </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,26 @@
 import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
+
 
 function Navbar() {
+    const [click,setclick]=useState(false);
+
+    const handleClick=()=> setclick(!click);
     return (
-        <div>
-           
-        </div>
+        <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    TRVL <i className='fab fa-typo3'></i>
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+            </div>
+
+        </nav>
+          
+        </>
     )
 }
 

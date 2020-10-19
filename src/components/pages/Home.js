0a7feb4f0ee636e82b7515f7481a71ea247efbb4
Shubham Gupta <61947842+starkex@users.noreply.github.com>
2020-10-19 15:14:31 +0530

    yo

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import '../../App.css';
 import Cards from '../Cards';
-import HeroSection from '../HeroSection';
-import Footer from '../Footer';
 
+import HeroSection from '../HeroSection';
 function Home() {
   return (
     <>
       <HeroSection />
-      <Cards />
-      <Footer />
+      <Cards/>
     </>
   );
 }

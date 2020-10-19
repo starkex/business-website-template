cf59b48e89b5dc03eec4aeeb77f72fef2b11ed8a
Shubham Gupta <61947842+starkex@users.noreply.github.com>
2020-10-19 15:25:46 +0530

    blocks-set

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -11,7 +11,7 @@ function CardItem(props) {
                       <img src ={props.src} alt="travel image" className="cards__item__img"/>
                   </figure>
                   <div className="cards__item__info">
-    <h5 className='cards__item__text'>{props.text}</h5>
+            <h5 className='cards__item__text'>{props.text}</h5>
                   </div>
               </Link>
               </li>  
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,7 +8,26 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                      <CardItem src="images/img-9.jpg" text="Explore some real world here now" label="adventure" path='/services'/>
+                      <CardItem src="images/img-9.jpg" 
+                      text="Explore some real world here now" 
+                      label="Adventure" 
+                      path='/services'/>
+                       <CardItem src="images/img-8.jpg" 
+                      text="Deserts here now" 
+                      label="Explore" 
+                      path='/services'/>
+                       <CardItem src="images/img-7.jpg" 
+                      text="Explore the Real You" 
+                      label="Reality" 
+                      path='/services'/>
+                       <CardItem src="images/img-6.jpg" 
+                      text="See how time controls the whole World" 
+                      label="Travel" 
+                      path='/services'/>
+                       <CardItem src="images/img-5.jpg" 
+                      text="Grand Luxuries are waiting for you" 
+                      label="Luxury" 
+                      path='/services'/>
                     </ul>
                 </div>
             </div>
